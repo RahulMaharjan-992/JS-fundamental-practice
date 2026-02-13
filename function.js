7974098025fc836a27ac1e5e26f0e468d21400e7
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-13 12:28:36 -0600

    Update function.js

--- function.js
+++ function.js
@@ -7,3 +7,22 @@ function costOfMonitors(rows, columns)
 }
 const totalCost = costOfMonitors(5,4);
 console.log(totalCost);
+
+//functions as parameters
+const addTwo = num => {
+  return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+const checkA = val + 2;
+const checkB = func(val);
+if (checkA===checkB)
+{
+  return (checkB);
+}
+else
+{return ('inconsistent results');}
+}
+
+console.log(checkConsistentOutput(addTwo,10));
+
