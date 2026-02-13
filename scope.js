3633bf8940b9195caaa2935ab1fdeb41db0216d5
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-13 11:56:20 -0600

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,33 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  if (region==='The Arctic')
+  {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+//global scope:
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+function callMyNightSky()
+{
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
+
+//block and scope
+const city = 'New York City';
+function logCitySkyline()
+
+{
+let skyscraper = 'Empire State Building';
+return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+console.log(logCitySkyline());
