d61654098c096ba2b309bf746f47f4e2c4ab3ee8
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-13 11:33:58 -0600

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,10 @@
+const firstMessage = 'I will print!';
+const secondMessage = 'I will not print!'; 
+
+do {
+ console.log(firstMessage)
+} while (true === false);
+
+while (true === false){
+  console.log(secondMessage)
+};
