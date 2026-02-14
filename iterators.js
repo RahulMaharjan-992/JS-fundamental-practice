8f4d2e4b4edf2f8a6f171152351049a2baf800e7
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-14 11:55:53 -0600

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -36,3 +36,17 @@ console.log(foundAnimal);
 const startsWithS = animals.findIndex
 (animal => animal.startsWith('s'));
 console.log(startsWithS);
+
+//.reduce
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce(
+(accumulator, currentValue) => 
+{
+console.log('The value of accumulator: ', accumulator);
+console.log('The value of currentValue: ', currentValue);
+return accumulator + currentValue;
+},
+10
+)
+console.log(newSum);
+
