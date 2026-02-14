1a111f47bbb34b91c689048e8607d36c2acbedd2
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-14 13:45:35 -0600

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,84 @@
+//making an object
+let apple =
+{
+  color :'red',
+  'taste' : 'sweet'
+}
+
+//bracket notation
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+let isActive = spaceship['Active Mission']
+console.log(spaceship[propName]);
+ 
+//property assignment
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold';
+spaceship['numEngines'] = 7;
+delete spaceship['Secret Mission'];
+
+//Methods
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+const alienShip = { retreat(retreatMessage){console.log('We no longer wish to conquer your planet. It is full of dogs, which we do not care for.');
+},
+takeOff() {console.log('Spim... Borp... Glix... Blastoff!');}
+}
+alienShip.retreat();
+alienShip.takeOff();
+
+//pass by reference 
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+let greenEnergy =
+spaceship => {spaceship['Fuel Type'] = 'avocado oil'}
+let remotelyDisable = spaceship => {
+  spaceship.disabled = true;
+}
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);
+
+//looping through objects
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+// Write your code below
+for (let crewMem in spaceship.crew)
+{
+  console.log(`${crewMem}: ${spaceship.crew[crewMem].name},\nrole: ${spaceship.crew[crewMem].degree}\n`);
+}
