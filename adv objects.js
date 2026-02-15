32d2b2573abfe08e92a6b3c736fe04b84851844c
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-15 12:04:14 -0600

    Create adv objects.js

---
+++ adv objects.js
@@ -0,0 +1,74 @@
+//this keyword
+const robot = {
+  model: '1E78V2',
+  energyLevel : 100,
+  provideInfo()
+  {
+    return (`I am ${this.model} and my current energy level is ${this.energyLevel}.`)
+  }
+};
+console.log(robot.provideInfo());
+
+//accessing and updating properties 
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+robot._energyLevel = 'high';
+console.log(robot.recharge());
+
+//getters
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: '100',
+get energyLevel()
+{
+  if (typeof this._energyLevel === 'number')
+  return (`My current energy level is ${this._energyLevel}`);
+else 
+return ('System malfunction: cannot retrieve energy level');
+}
+};
+console.log(robot.energyLevel);
+
+//setters
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) 
+  {
+if (typeof num === 'number' && num >= 0)
+{this._numOfSensors = num;}
+  else 
+  console.log('Pass in a number that is greater than or equal to 0');
+  }
+   
+};
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+//factory functions
+const robotFactory = (model, mobile) => 
+{
+  return {
+  model: model,
+  mobile: mobile,
+  beep() {
+  console.log('Beep Boop');
+}
+}
+}
+ const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
