3dd2f98c8c53527791fbe6126f17be371f5a721a
RahulMaharjan-992 <rahulmaharjan992@gmail.com>
2026-02-19 12:42:54 -0600

    Update asynchronous porgrams.js

--- asynchronous porgrams.js
+++ asynchronous porgrams.js
@@ -33,3 +33,59 @@ async function announceDinner() {
   console.log(`I'm going to make ${meal} for dinner.`);
 }
 announceDinner();
+
+//async function
+const shopForBeans = require('./library.js');
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value =  await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
+
+//handling dependent promises 
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+async function makeBeans(){
+  const type = await shopForBeans();
+  const isSoft = await soakTheBeans(type);
+  const dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+makeBeans();
+
+//handling errors
+const cookBeanSouffle = require('./library.js');
+async function hostDinnerParty(){
+  try{
+    let dinner = await cookBeanSouffle();
+console.log(`${dinner} is served!`);
+  }
+  catch(error){
+    console.log(error);
+console.log('Ordering a pizza!');
+  }
+} 
+hostDinnerParty();
+
+//handling dependent promises 
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+async function serveDinner(){
+  const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice();
+  const proteinPromise = bakeChicken();
+  const sidePromise = cookBeans();
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+}
+serveDinner();
+
+//await promise.all
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+async function serveDinnerAgain(){
+  let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+
+  console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`);
+}
+serveDinnerAgain();
+
+
